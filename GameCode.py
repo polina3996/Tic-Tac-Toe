22b7e47f405410ef0ab4969d0212f1aae146aacf
PalinaK <polina.kirichencko@yandex.ru>
2023-12-10 19:43:09 +0100

    Add adbility to choose playing figure

--- GameCode.py
+++ GameCode.py
@@ -1,24 +1,26 @@
 import tkinter
 import random
-import time
+
 
 class TicTacToeCanvas(tkinter.Canvas):
     def __init__(self, window):
         super().__init__(window, width=300, height=300)
         self.nullify_state()
         self.game_field = {0:(0,0), 1:(1,0), 2:(2,0), 3:(0,1), 4:(1,1), 5:(2,1), 6:(0,2), 7:(1,2), 8:(2,2)}
-        self.pack()
-        self.draw_lines()
-        self.bind('<Button-1>', self.click)    
-   
+        self.pack()  
+        self.start_game()        
+           
     def nullify_state(self):
         self.state = [None,None,None,None,None,None,None,None,None]
         
     #processing of clicking on canvas method
     def click(self, event):
+        if not self.is_game_started:
+            self.handle_select_symbol(event)
+            return
         if (self.get_winner()):
             self.delete('all') 
-            self.restart_game()
+            self.start_game()
             return
         x = event.x  
         y = event.y 
@@ -27,23 +29,26 @@ class TicTacToeCanvas(tkinter.Canvas):
         i = column + row*3 
         if self.state[i] != None:
             return 
-        self.state[i] = 'x'
-        self.draw_x(column, row)
+        self.state[i] = self.player_symbol
+        self.draw_player_symbol(column, row)
         result = self.get_winner()
         if result == None:
             self.bot_move()
             result = self.get_winner()
         if result != None:
-            self.create_text(150, 150, text=f"{result}. Click anywhere to start new game", fill='blue')
+            self.create_text(150, 150, text=f"{result}. Click anywhere on the field to start a new game", fill='blue')
             return
     
-    def restart_game(self):
-        self.nullify_state()
-        self.create_text(150,150,text='restarting game...',fill='blue')
-        
+    def start_game(self):
+        self.nullify_state()     
         self.delete('all')
         self.update()
-        self.draw_lines()        
+        self.draw_x(0, 1)
+        self.draw_o(2, 1)
+        self.draw_lines()
+        self.bind('<Button-1>', self.click) 
+        self.is_game_started = False
+        self.create_text(150, 150, text=f"Please select X or O", fill='blue')      
        
     def get_empty_cells_indexes(self):
         list_of_indexes = []
@@ -55,9 +60,33 @@ class TicTacToeCanvas(tkinter.Canvas):
     def bot_move(self): 
         list_of_indexes = self.get_empty_cells_indexes()
         random_index = random.choice(list_of_indexes) 
-        self.state[random_index] = 'o' 
+        self.state[random_index] = self.bot_symbol
         column, row = self.game_field[random_index]
-        self.draw_o(column, row) 
+        self.draw_bot_symbol(column, row) 
+    
+    def handle_select_symbol(self, event):
+        x = event.x  
+        y = event.y 
+        column = x // 100
+        row = y // 100
+        if column == 0 and row == 1:
+            self.draw_player_symbol = self.draw_x 
+            self.draw_bot_symbol = self.draw_o
+            self.player_symbol = 'X'
+            self.bot_symbol = 'O'
+            self.is_game_started = True
+            self.delete('all')
+            self.update()                
+            self.draw_lines()
+        elif column == 2 and row == 1:
+            self.draw_player_symbol = self.draw_o
+            self.draw_bot_symbol = self.draw_x
+            self.player_symbol = 'O'
+            self.bot_symbol = 'X'
+            self.is_game_started = True
+            self.delete('all')                
+            self.update()                
+            self.draw_lines()
         
     #drawing the lines dividing the field into cells
     def draw_lines(self):
@@ -88,10 +117,10 @@ class TicTacToeCanvas(tkinter.Canvas):
             for i in winner_combination:
                 condition = self.state[i]
                 conditions.append(condition)
-            if conditions == ['x', 'x', 'x']:
-                return 'x_win'
-            elif conditions == ['o', 'o', 'o']:
-                return 'o_win'
+            if conditions == ['X', 'X', 'X']:
+                return 'X wins!'
+            elif conditions == ['O', 'O', 'O']:
+                return 'O wins!'
         for i in self.state:
             if i == None:
                 return None 
